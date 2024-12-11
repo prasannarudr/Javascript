1aff6a7e8f1b8f4091853048596001b0c08cc8ff
prasannarudr <rudrapmishra_en24b18_117@dtu.ac.in>
2024-12-11 19:06:56 +0000

    commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,5 +3,8 @@ let accountEmail = "prasannarudra@gmail.com"
 var accountPassword = "112233"
 accountCity = "Delhi"
 // accountId = 2333 // not allowed due to value being constant
-
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
 console.table([accountId, accountEmail, accountPassword, accountCity])
