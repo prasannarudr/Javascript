4454c28c15027ceb01251357c9917a772729d766
prasannarudr <rudrapmishra_en24b18_117@dtu.ac.in>
2024-12-11 19:04:12 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1233344
+let accountEmail = "prasannarudra@gmail.com"
+var accountPassword = "112233"
+accountCity = "Delhi"
+// accountId = 2333 // not allowed due to value being constant
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
