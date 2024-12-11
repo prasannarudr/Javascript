d1081fb8a3a3d573616e1e71271c4095d44af446
prasannarudr <rudrapmishra_en24b18_117@dtu.ac.in>
2024-12-11 20:00:56 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //Treats all JS code as newer version
+
+// alert(3 + 3) //we use node.js, not browser
+
+console.log(3+3) //code readability should be high
+console.log("Rudra")
+let name = "Rudra"
+let age = 19
+let isLoggedIn = false
+
+
+//number => 2 etc etc
+// bigint
+// string => ""
+// null => standalone value
+// undefined => 
+// boolean => true/false
+// symbol => unique
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
