c2bd8c5804921a4c6de6658bb4272d58d6668381
prasannarudr <rudrapmishra_en24b18_117@dtu.ac.in>
2024-12-12 08:08:57 +0000

    commit

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+let score = "rudra"
+
+console.log(typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+
+/*  "33" => 33
+"33abc" => NaN (Not A Number)
+true = 1; false = 0
+*/
+
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+/* 1 = true; 0 = false
+"" = false
+"Rudra" = true
+*/
+
+let somenumber = 81
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
