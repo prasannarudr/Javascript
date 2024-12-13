c0408fb96b4535b52c64b66348f35ce90aac382b
prasannarudr <rudrapmishra_en24b18_117@dtu.ac.in>
2024-12-13 06:47:05 +0000

    commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+
+console.log(null >= 0); 
+
+/* Comparisons convert null to a number, treating it as 0. That's why null >= 0 is true
+*/
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check
+
+console.log("22" === 22);
