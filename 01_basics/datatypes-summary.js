f8b0a2c06c1541e97a0fc679b624210e7ddfe663
prasannarudr <rudrapmishra_en24b18_117@dtu.ac.in>
2024-12-16 07:12:27 +0000

    commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,62 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+//*****************************************
+
+/* Stack - Primitive
+Heap - Non Primitive
+*/
+let myname = "Rudra"
+
+let anothername = myname
+anothername = "Sunny"
+
+console.log(anothername);
+console.log(myname);
+
+let userOne = {
+    email: "user@google.com",
+    UPI: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email= "rudraprasanna@google.com"
+
+console.log(userTwo.email);
+console.log(userOne.email);
+
